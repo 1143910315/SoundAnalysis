d13096274b0565b09ac07ce9a90e1b36d55b6618
HapiFive <1143910315@qq.com>
2022-02-26 17:29:15 +0800

    跳转主目录改为历史回退，并仿链接

--- src/components/TensorFlowTest.vue
+++ src/components/TensorFlowTest.vue
@@ -1,10 +1,28 @@
 <template>
-    <router-link to="/">返回主目录</router-link>
+    <div @click="backPage()" class="link">返回主目录</div>
     <div>TensorFlow测试页</div>
 </template>
 
 <script>
+    export default {
+        methods: {
+            backPage() {
+                this.$router.go(-1);
+            }
+        }
+    }
 </script>
 
 <style>
+    .link {
+        color: -webkit-link;
+        cursor: pointer;
+        text-decoration: underline;
+    }
+    .link:focus-visible {
+        outline-offset: 1px;
+    }
+    .link:active {
+        color: -webkit-activelink;
+    }
 </style>
